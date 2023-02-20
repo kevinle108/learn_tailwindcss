32fa42e95c37768205b628ebb03e266f5dd5a2ca
Kevin Le <kevinle108@gmail.com>
2023-02-20 17:40:21 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<nav class="container relative mx-auto p-6">
+	<!-- Flex container -->
+	<div class="flex items-center justify-between">
+		<!-- Logo -->
+		<div class="pt-2">
+			<img src="../src/img/logo.svg" alt="logo" />
+		</div>
+		<!-- Menu Items -->
+		<div class="hidden space-x-6 md:flex">
+			<a href="#" class="hover:text-darkGrayishBlue">Pricing</a>
+			<a href="#" class="hover:text-darkGrayishBlue">Product</a>
+			<a href="#" class="hover:text-darkGrayishBlue">About Us</a>
+			<a href="#" class="hover:text-darkGrayishBlue">Careers</a>
+			<a href="#" class="hover:text-darkGrayishBlue">Community</a>
+		</div>
+		<!-- Button -->
+		<a href="#" class="hidden p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight md:block">Get Started</a>
+	</div>
+</nav>
